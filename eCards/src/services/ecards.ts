91a44baafd5ee11ea8f6ab55b671dfe7654ef170
Luis Carlos Pando <luiscarlos@luiscarlospando.net>
2018-09-14 10:21:38 -0600

    Crear servicio para manejar estado de Favoritos

---
+++ eCards/src/services/ecards.ts
@@ -0,0 +1,26 @@
+import { Ecard } from "../data/ecard.interface";
+
+export class EcardsService {
+    // Lugar donde guardar eCards Favoritas
+    private ecardsFavoritas: Ecard[] = [];
+
+    // Métodos
+    agregarEcardAFavoritos(ecard: Ecard) {
+        this.ecardsFavoritas.push(ecard);
+    };
+
+    quitarEcardDeFavoritos(ecard: Ecard) {
+        const posicion = this.ecardsFavoritas.findIndex((ecardElemento: Ecard) => {
+            // Checamos si el elemento que pasamos a la función findIndex coincide con la eCard que queremos quitar
+            return ecardElemento.id == ecard;
+        });
+
+        // Ahora si eliminamos sabiendo la posición del elemento usando el método splce
+        this.ecardsFavoritas.splice(posicion, 1);
+    };
+
+    obtenerEcardsFavoritas() {
+        // Retornamos una copia del arreglo original usando slice sin argumentos
+        return this.ecardsFavoritas.slice();
+    }
+}
