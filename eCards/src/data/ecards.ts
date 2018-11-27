6bc77d6bd05dbfafc579046627e3c35ca1be0c00
Luis Carlos Pando <luiscarlos@luiscarlospando.net>
2018-11-27 10:39:34 -0700

    Modificar estructura de datos con eCards provisionales

--- eCards/src/data/ecards.ts
+++ eCards/src/data/ecards.ts
@@ -1,87 +1,45 @@
 const ecardsData = [
     {
-        categoria: 'Cumpleaños para una amiga',
+        categoria: 'Amistad',
         ecards: [
             {
                 id: 1,
-                titulo: 'Feliz Cumpleaños 1',
-                texto: 'Me hace muy feliz poder compartir este gran día contigo ya que no eres solo una amiga, sino que eres como mi hermana. Muchas felicidades, amiga, espero que lo pases en grande.',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/birthday-party.jpg'
-            },
+                titulo: 'Amistad 1',
+                texto: '',
+                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/amistad/dee_amistad_01.jpg'
+            }
+        ],
+        icono: 'trebol'
+    },
+    {
+        categoria: 'Cumpleaños',
+        ecards: [
             {
                 id: 2,
-                titulo: 'Feliz Cumpleaños 2',
-                texto: 'A lo largo de la vida he conocido a muchas personas, unas se han quedado y otras se han ido, pero tú, amiga mía, has estado junto a mí durante todos estos años. Es por eso por lo que quiero desearte un muy feliz cumpleaños. Espero que nuestra amistad dure para toda la vida.',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/birthday-party.jpg'
-            },
-            {
-                id: 3,
-                titulo: 'Feliz Cumpleaños 3',
-                texto: 'Nunca olvidaré las noches que hemos pasado en vela juntas entre libros y películas. Te quiero mucho, amiga. ¡Feliz cumpleaños!',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/birthday-party.jpg'
-            },
-            {
-                id: 4,
-                titulo: 'Feliz Cumpleaños 4',
-                texto: 'Cumplas los años que cumplas, son los nuevos 30, ¡felicidades!',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/birthday-party.jpg'
+                titulo: 'Feliz Cumpleaños 1',
+                texto: '',
+                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/cumple/dee_cumple_01.jpg'
             }
         ],
         icono: 'estrella'
     },
     {
-        categoria: 'Cumpleaños para un amigo',
+        categoria: 'Navidad',
         ecards: [
+            {
+                id: 3,
+                titulo: 'Feliz Navidad y Prospero Año Nuevo 1',
+                texto: '',
+                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/navidad/dee_navidad_600x600_01.gif'
+            },
             {
                 id: 4,
-                titulo: 'Feliz Cumpleaños 1',
-                texto: 'El hecho de ser tan diferentes no nos ha impedido ser amigos desde pequeños y pase lo que pase nada nos impedirá celebrar hoy tu cumpleaños como manda la tradición. ¡Felicidades, amigo! Disfruta de todas las sorpresas que te deparará el día.',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/birthday-party.jpg'
-            },
-            {
-                id: 5,
-                titulo: 'Feliz Cumpleaños 2',
-                texto: 'Después de pensar durante un buen rato no he sido capaz de encontrar las palabras adecuadas para poder escribirte una bonita felicitación después de tantos años de amistad y es que, ¿Qué hay más bonito que demostrar la amistad cada día? Creo que nada. Así que, sin más, te deseo un feliz cumpleaños, amigo mío. ¡Brindemos por muchos años más juntos!',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/birthday-party.jpg'
-            },
-            {
-                id: 6,
-                titulo: 'Feliz Cumpleaños 3',
-                texto: 'Me gustaría mucho que supieras que hasta que no te conocí no pude disfrutar de la satisfacción de tener a un buen amigo a mi lado. Hoy quiero ser yo el que esté a tu lado para disfrutar y celebrar contigo este día tan especial. Feliz cumpleaños, ¡Gracias por tantos años de amistad!',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/birthday-party.jpg'
-            },
-            {
-                id: 7,
-                titulo: 'Feliz Cumpleaños Animado',
-                texto: 'Me gustaría mucho que supieras que hasta que no te conocí no pude disfrutar de la satisfacción de tener a un buen amigo a mi lado. Hoy quiero ser yo el que esté a tu lado para disfrutar y celebrar contigo este día tan especial. Feliz cumpleaños, ¡Gracias por tantos años de amistad!',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/feliz-cumpleanos-animado.gif'
+                titulo: 'Feliz Navidad y Prospero Año Nuevo 2',
+                texto: '',
+                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/navidad/dee_navidad_600x600_02.gif'
             }
         ],
-        icono: 'corona'
-    },
-    {
-        categoria: 'Día de la Madre',
-        ecards: [
-            {
-                id: 8,
-                titulo: 'Todo lo que soy',
-                texto: 'Todo lo que soy y espero ser, se lo debo a la angelical solicitud de mi madre.',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/mothers-day.jpeg'
-            },
-            {
-                id: 9,
-                titulo: 'El corazón de una madre',
-                texto: 'El corazón de una madre es el único capital del sentimiento que nunca quiebra, y con el cual se puede contar siempre y en todo tiempo con toda seguridad.',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/mothers-day.jpeg'
-            },
-            {
-                id: 10,
-                titulo: 'Una madre perdona',
-                texto: 'Una madre perdona siempre; ha venido al mundo para esto.',
-                img: 'http://interactivesolutions.info/dee-ecards-app/assets/imgs/mothers-day.jpeg'
-            }
-        ],
-        icono: 'foco'
+        icono: 'hojas'
     }
 ]
 export default ecardsData;
