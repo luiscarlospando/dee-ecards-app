7e3c87255887f1513733676badb9800f4f42612e
Luis Carlos Pando <luiscarlos@luiscarlospando.net>
2018-12-10 17:17:10 -0700

    Agregar eCards faltantes

--- eCards/src/data/ecards.ts
+++ eCards/src/data/ecards.ts
@@ -4,21 +4,63 @@ const ecardsData = [
         ecards: [
             {
                 id: 1,
-                titulo: 'Abrazo de Oso',
+                titulo: 'Amistad 1',
                 texto: '',
                 img: 'http://discoveryenlaescuela.com/dee-ecards-app/assets/imgs/amistad/dee_amistad_01.png'
+            },
+            {
+                id: 2,
+                titulo: 'Amistad 2',
+                texto: '',
+                img: 'http://discoveryenlaescuela.com/dee-ecards-app/assets/imgs/amistad/dee_amistad_02.jpg'
+            },
+            {
+                id: 3,
+                titulo: 'Amistad 3',
+                texto: '',
+                img: 'http://discoveryenlaescuela.com/dee-ecards-app/assets/imgs/amistad/dee_amistad_03.jpg'
+            },
+            {
+                id: 4,
+                titulo: 'Amistad 4',
+                texto: '',
+                img: 'http://discoveryenlaescuela.com/dee-ecards-app/assets/imgs/amistad/dee_amistad_04.jpg'
             }
         ],
         icono: 'trebol'
     },
     {
+        categoria: 'Amor',
+        ecards: [
+            {
+                id: 5,
+                titulo: 'Amor 1',
+                texto: '',
+                img: 'http://discoveryenlaescuela.com/dee-ecards-app/assets/imgs/amor/dee_amor_01.jpg'
+            },
+            {
+                id: 6,
+                titulo: 'Amor 2',
+                texto: '',
+                img: 'http://discoveryenlaescuela.com/dee-ecards-app/assets/imgs/amor/dee_amor_02.jpg'
+            }
+        ],
+        icono: 'corazon'
+    },
+    {
         categoria: 'Cumpleaños',
         ecards: [
             {
-                id: 2,
-                titulo: 'Feliz Cumpleaños 1',
+                id: 7,
+                titulo: 'Cumpleaños 1',
                 texto: '',
                 img: 'http://discoveryenlaescuela.com/dee-ecards-app/assets/imgs/cumple/dee_cumple_01.png'
+            },
+            {
+                id: 8,
+                titulo: 'Cumpleaños 2',
+                texto: '',
+                img: 'http://discoveryenlaescuela.com/dee-ecards-app/assets/imgs/cumple/dee_cumple_02.jpg'
             }
         ],
         icono: 'estrella'
@@ -27,13 +69,13 @@ const ecardsData = [
         categoria: 'Navidad',
         ecards: [
             {
-                id: 3,
+                id: 9,
                 titulo: 'Feliz Navidad y Prospero Año Nuevo 1',
                 texto: '',
                 img: 'http://discoveryenlaescuela.com/dee-ecards-app/assets/imgs/navidad/dee_navidad_600x600_01.gif'
             },
             {
-                id: 4,
+                id: 10,
                 titulo: 'Feliz Navidad y Prospero Año Nuevo 2',
                 texto: '',
                 img: 'http://discoveryenlaescuela.com/dee-ecards-app/assets/imgs/navidad/dee_navidad_600x600_02.gif'
