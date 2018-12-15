0c4b237c1b8bef2f657ae1283ff18ae692b77140
Luis Carlos Pando <luiscarlos@luiscarlospando.net>
2018-12-14 18:26:04 -0700

    Se agregaron disclaimers a páginas de Acerca de y página de envío de eCard

--- eCards/src/pages/acerca-de/acerca-de.ts
+++ eCards/src/pages/acerca-de/acerca-de.ts
@@ -15,7 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AcercaDePage {
   
-  titulo: string = "Discovery en la Escuela eCards v1.0.0";
+  titulo: string = "Discovery en la Escuela eCards v1.0.1";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
